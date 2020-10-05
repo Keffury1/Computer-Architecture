f5ea5899f388ffc8f9d29103294026ec16fde5fc
Robert Keffury <bobby.keffury@me.com>
2020-10-05 10:42:41 -0700

    Day 2 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,6 +4,7 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
 
@@ -24,18 +25,26 @@ class CPU:
 
         address = 0
 
-        program = [
-            0b10000010,
-            0b00000000,
-            0b00001000,
-            0b01000111,
-            0b00000000,
-            0b00000001,
-        ]
+        if (len(sys.argv)) != 2:
+            print("Pass in second file name")
+            sys.exit()
+ 
+        try:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    split = line.split('#')
+                    value = split[0].strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if value == '':
+                        continue
+                    
+                    number = int(value[0])
+                    self.ram[address] = number
+                    address += 1
+        except FileNotFoundError:
+            print(f'{sys.argv[1]} file not found')
+            sys.exit()
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -76,5 +85,8 @@ class CPU:
                 self.pc += 2
             elif instruction == LDI:
                 self.registers[a] = b
+                self.pc += 3
+            elif instruction == MUL:
+                self.reg[a] = self.reg[a] * self.reg[b]
                 self.pc += 3
 
