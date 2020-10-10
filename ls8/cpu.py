eb34324681cd5a9669285e3ae8da902c2233ee07
Robert Keffury <bobby.keffury@me.com>
2020-10-09 19:33:40 -0700

    SC MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,10 @@ PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 class CPU:
 
@@ -17,6 +21,7 @@ class CPU:
         self.registers = [0] * 8
         self.pc = 0
         self.sp = 7
+        self.flag = 0B00000000
         
 
     def ram_read(self, address):
@@ -36,7 +41,6 @@ class CPU:
             address = 0
             with open(sys.argv[1]) as file:
                 for line in file:
-                    print(line)
                     split = line.split('#')
                     value = split[0].strip()
 
@@ -60,7 +64,14 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.registers[reg_a] += self.registers[reg_b]
+        elif op == "CMP":
+            if self.registers[reg_a] < self.registers[reg_b]:
+                self.flag = 0b00000100
+            if self.registers[reg_a] > self.registers[reg_b]:
+                self.flag = 0b00000010
+            if self.registers[reg_a] == self.registers[reg_b]:
+                self.flag = 0b00000001
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -115,12 +126,30 @@ class CPU:
                 self.pc += 2
             elif instruction == CALL:
                 return_address = self.pc + 2
-                register = self.ram[self.pc + 1]
-                subroutine_address = self.registers[register]
                 self.registers[self.sp] -= 1
                 self.ram[self.registers[self.sp]] = return_address
+
+                register = self.ram[self.pc + 1]
+                subroutine_address = self.registers[register]
                 self.pc = subroutine_address
             elif instruction == RET:
                 return_address = self.registers[self.sp]
                 self.pc = self.ram[return_address]
                 self.registers[self.sp] += 1
+            elif instruction == CMP:
+                self.alu("CMP", a, b)
+                self.pc += 3
+            elif instruction == JMP:
+                self.pc = self.registers[a]
+            elif instruction == JEQ:
+                if self.flag == 0b00000001:
+                    self.pc = self.registers[a]
+                else:
+                    self.pc += 2
+            elif instruction == JNE:
+                if self.flag != 0b00000001:
+                    self.pc = self.registers[a]
+                else:
+                    self.pc += 2
+            else:
+                self.pc += 1
